2ea2352f328d1ffe7da317bb985f0e069db67992
boutr <boutr@MSI>
2016-05-22 02:32:16 +0200

    Update

--- src/com/skyzen/smashsgames/object/Scoreboards.java
+++ src/com/skyzen/smashsgames/object/Scoreboards.java
@@ -1,5 +1,6 @@
 package com.skyzen.smashsgames.object;
 
+import com.skyzen.smashsgames.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.DisplaySlot;
@@ -12,36 +13,42 @@ public class Scoreboards {
     private static String ip[] = {"§b§lplay.smashs.net", "§b§lplay.smashs.net", "§b§lplay.smashs.net", "§b§lplay.smashs.net", "§b§lplay.smashs.net", "§b§lplay.smashs.net", "§b§lplay.smashs.net", "§b§lplay.smashs.net", "§b§lplay.smashs.net", "§b§lplay.smashs.net",};
 
     public static void build(boolean deconnection) {
-        Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+        new Runnable() {
+            public int taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.instance, this, 0L, 5L);
 
-        Objective obj = board.registerNewObjective("dummy", "title");
-        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-        obj.setDisplayName("§7☢ §b§lSmashsBuild §7☢");
+            @Override
+            public void run() {
+                Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
 
-        obj.getScore(" ").setScore(10);
+                Objective obj = board.registerNewObjective("dummy", "title");
+                obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+                obj.setDisplayName("§7☢ §b§lSmashsBuild §7☢");
 
-        obj.getScore("§b§lObjectif Principal:").setScore(9);
-        obj.getScore("§fCarte de ElytraRace").setScore(8);
+                obj.getScore(" ").setScore(10);
 
-        obj.getScore("  ").setScore(7);
+                obj.getScore("§b§lObjectif Principal:").setScore(9);
+                obj.getScore("§fCarte de ElytraRace").setScore(8);
 
-        obj.getScore("§b§lJoueurs:").setScore(6);
-        if (deconnection)
-            obj.getScore("§f" + (Bukkit.getOnlinePlayers().size() - 1) + "/" + Bukkit.getMaxPlayers()).setScore(5);
-        else
-            obj.getScore("§f" + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers()).setScore(4);
+                obj.getScore("  ").setScore(7);
 
-        obj.getScore("   ").setScore(2);
+                obj.getScore("§b§lJoueurs:").setScore(6);
+                if (deconnection)
+                    obj.getScore("§f" + (Bukkit.getOnlinePlayers().size() - 1) + "/" + Bukkit.getMaxPlayers()).setScore(5);
+                else
+                    obj.getScore("§f" + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers()).setScore(4);
 
-        obj.getScore(ip[ipID]).setScore(1);
+                obj.getScore("   ").setScore(2);
 
-        for (Player online : Bukkit.getOnlinePlayers())
-            online.setScoreboard(board);
+                obj.getScore(ip[ipID]).setScore(1);
 
-        ipID++;
-        if (ipID > ip.length - 1) {
-            ipID = 0;
-        }
+                for (Player online : Bukkit.getOnlinePlayers())
+                    online.setScoreboard(board);
+
+                ipID++;
+                if (ipID > ip.length - 1) {
+                    ipID = 0;
+                }
+            }
+        };
     }
-
 }
