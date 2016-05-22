5cd824d537aea8d67fc4e097edd996b7c303994c
boutr <boutr@MSI>
2016-05-22 02:49:43 +0200

    Update

--- src/com/skyzen/smashsgames/object/Scoreboards.java
+++ src/com/skyzen/smashsgames/object/Scoreboards.java
@@ -11,10 +11,12 @@ public class Scoreboards {
 
     private static int ipID = 0;
     private static String ip[] = {"§ep§6lay.smashs.net", "§fp§el§6ay.smashs.net", "§ep§fl§ea§6y.smashs.net", "§6p§el§fa§ey§6.smashs.net", "§6pl§ea§fy§e.§6smashs.net", "§6pla§ey§f.§es§6mashs.net", "§6play§e.§fs§em§6ashs.net", "§6play.§es§fm§ea§6shs.net", "§6play.s§em§fa§es§6hs.net", "§6play.sm§ea§fs§eh§6s.net", "§6play.sma§es§fh§es§6.net", "§6play.smas§eh§fs§e.§6net", "§6play.smash§es§f.§en§6et", "§6play.smashs§e.§fn§ee§6t", "§6play.smashs.§en§fe§et", "§ep§6lay.smashs.n§ee§ft", "§fp§el§6ay.smashs.ne§et"};
+    private static int titleID = 0;
+    private static String title[] = {"§7☢ §b§lSmashsBuild §7☢"};
 
     public static void build(boolean deconnection) {
         new Runnable() {
-            public int taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.instance, this, 0L, 2L);
+            public int taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.instance, this, 0L, 5L);
 
             @Override
             public void run() {
@@ -22,16 +24,16 @@ public class Scoreboards {
 
                 Objective obj = board.registerNewObjective("dummy", "title");
                 obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-                obj.setDisplayName("§7☢ §6§lSmashsBuild §7☢");
+                obj.setDisplayName(title[titleID]);
 
                 obj.getScore(" ").setScore(10);
 
-                obj.getScore("§6§lObjectif Principal:").setScore(9);
+                obj.getScore("§b§lObjectif Principal:").setScore(9);
                 obj.getScore("§fCarte de ElytraRace").setScore(8);
 
                 obj.getScore("  ").setScore(7);
 
-                obj.getScore("§6§lJoueurs:").setScore(6);
+                obj.getScore("§b§lJoueurs:").setScore(6);
                 if (deconnection)
                     obj.getScore("§f" + (Bukkit.getOnlinePlayers().size() - 1) + "/" + Bukkit.getMaxPlayers()).setScore(5);
                 else
@@ -47,6 +49,10 @@ public class Scoreboards {
                 ipID++;
                 if (ipID > ip.length - 1) {
                     ipID = 0;
+                }
+                titleID++;
+                if (titleID > title.length - 1) {
+                    titleID = 0;
                 }
             }
         };
