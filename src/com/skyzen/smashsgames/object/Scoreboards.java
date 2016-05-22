a4937ea45e599ae469186970785a6b377913a352
BuildTools <unconfigured@null.spigotmc.org>
2016-05-22 02:01:48 +0200

    Initial commit

---
+++ src/com/skyzen/smashsgames/object/Scoreboards.java
@@ -0,0 +1,41 @@
+package com.skyzen.smashsgames.object;
+
+import com.skyzen.smashsgames.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+
+public class Scoreboards {
+
+    public static void build(boolean deconnection) {
+
+        Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+
+        Objective obj = board.registerNewObjective("dummy", "title");
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        obj.setDisplayName("§7☢ §b§lSmashsBuild §7☢");
+
+        obj.getScore(" ").setScore(10);
+
+        obj.getScore("§b§lObjectif Principal:").setScore(9);
+        obj.getScore("§fCarte de ElytraRace").setScore(8);
+
+        obj.getScore("  ").setScore(7);
+
+        obj.getScore("§b§lJoueurs:").setScore(6);
+        if (deconnection)
+            obj.getScore("§f" + (Bukkit.getOnlinePlayers().size() - 1) + "/" + Bukkit.getMaxPlayers()).setScore(5);
+        else
+            obj.getScore("§f" + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers()).setScore(4);
+
+        obj.getScore("   ").setScore(2);
+
+        obj.getScore("§b§lplay.smashs.net").setScore(1);
+
+        for (Player online : Bukkit.getOnlinePlayers())
+            online.setScoreboard(board);
+    }
+
+}
---
+++ src/com/skyzen/smashsgames/utils/ItemModifier.java
@@ -0,0 +1,21 @@
+package com.skyzen.smashsgames.utils;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+public class ItemModifier {
+
+    public static ItemStack setText(ItemStack item, String name, String... lore) {
+        ItemMeta meta = (ItemMeta) item.getItemMeta();
+        meta.setDisplayName(name);
+        meta.setLore(Arrays.asList(lore));
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    public static String getName(ItemStack item) {
+        return item.getItemMeta().getDisplayName();
+    }
+}
